20f63c830e260360d656c6649f10fd9021f3ca13
jesssekeogh <jesse.keogh74@gmail.com>
2025-04-10 19:20:29 +0100

    Wallet receive info

--- src/neuron_pylon_ui_frontend/src/components/wallet/RecieveToken.tsx
+++ src/neuron_pylon_ui_frontend/src/components/wallet/RecieveToken.tsx
@@ -1,12 +1,5 @@
-import {
-  Text,
-  Flex,
-  Spacer,
-  Button,
-} from "@chakra-ui/react";
-import {
-  MenuItem,
-} from "@/components/ui/menu";
+import { Text, Flex, Spacer, Button, Alert } from "@chakra-ui/react";
+import { MenuItem } from "@/components/ui/menu";
 import {
   DialogBody,
   DialogCloseTrigger,
@@ -20,9 +13,7 @@ import {
 } from "@/components/ui/dialog";
 import { ClipboardButton, ClipboardRoot } from "@/components/ui/clipboard";
 import { BiReceipt, BiCollapse } from "react-icons/bi";
-import {
-  stringToIcrcAccount,
-} from "@/utils/AccountTools";
+import { stringToIcrcAccount } from "@/utils/AccountTools";
 import { AccountIdentifier, SubAccount } from "@dfinity/ledger-icp";
 
 const RecieveToken = ({
@@ -67,6 +58,25 @@ const RecieveToken = ({
           <DialogTitle>Recieve {tokenSymbol}</DialogTitle>
         </DialogHeader>
         <DialogBody>
+          {tokenSymbol === "ICP" ? (
+            <>
+              <Flex align="center" mb={3}>
+                <Text fontWeight={500}>Account ID</Text>
+                <Spacer />
+                <ClipboardRoot value={icpAddress}>
+                  <ClipboardButton
+                    variant="surface"
+                    rounded="md"
+                    boxShadow="xs"
+                    size="2xs"
+                  />
+                </ClipboardRoot>
+              </Flex>
+              <Flex bg={"bg.muted"} borderRadius="md" p={3} mb={6}>
+                <Text lineClamp={4}>{icpAddress}</Text>
+              </Flex>
+            </>
+          ) : null}
           <Flex align="center" mb={3}>
             <Text fontWeight={500}>ICRC Account</Text>
             <Spacer />
@@ -82,25 +92,15 @@ const RecieveToken = ({
           <Flex bg={"bg.muted"} borderRadius="md" p={3}>
             <Text lineClamp={4}>{account}</Text>
           </Flex>
-          {tokenSymbol === "ICP" ? (
-            <>
-              <Flex align="center" mb={3} mt={6}>
-                <Text fontWeight={500}>Account ID</Text>
-                <Spacer />
-                <ClipboardRoot value={icpAddress}>
-                  <ClipboardButton
-                    variant="surface"
-                    rounded="md"
-                    boxShadow="xs"
-                    size="2xs"
-                  />
-                </ClipboardRoot>
-              </Flex>
-              <Flex bg={"bg.muted"} borderRadius="md" p={3}>
-                <Text lineClamp={4}>{icpAddress}</Text>
-              </Flex>
-            </>
-          ) : null}
+          <Alert.Root mt={3} variant={"outline"}>
+            <Alert.Indicator />
+            <Alert.Content>
+              <Alert.Description>
+                Wallets that support sending to these ICRC Accounts include the
+                NNS, Oisy and SwapRunner.
+              </Alert.Description>
+            </Alert.Content>
+          </Alert.Root>
         </DialogBody>
         <DialogFooter>
           <DialogActionTrigger asChild>
